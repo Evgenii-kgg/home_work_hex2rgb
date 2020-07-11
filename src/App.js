05a6c27a2ad4dc25b46ff9bc3bbf02ba65cdb324
Солодов Евгений Александрович <evgenii.kgg05@gmail.com>
2020-07-11 13:20:42 +0700

    fix

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      hex: "#FFFFFF",
+      rgb: "255,255,255",
+    };
+  }
+
+  checkHex = (hex) => {
+    if(hex.length != 7) return;
+    const hexRegex = /^[#]*([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/i;
+    if (hexRegex.test(hex)) {
+      return true;
+    } else {
+      this.setState({
+        rgb: `Ошибка`,
+      });
+      return false;
+    }
+  }
+
+  hexToRgb = (hex) => {
+    if (hex.length >7) return;
+    this.setState({ hex });
+    const chek = this.checkHex(hex)
+    if (chek) {
+      const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+      this.setState({
+        rgb: `rgb(${parseInt(result[1], 16)}, ${parseInt(
+          result[2],
+          16
+        )}, ${parseInt(result[3], 16)})`,
+      });
+    }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <div
+          className="content"
+          style={{ backgroundColor: `${this.state.rgb}` }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <input
+            type="text"
+            id="hex"
+            placeholder="hex"
+            value={this.state.hex}
+            onChange={(event) => this.hexToRgb(event.target.value)}
+          />
+          <br />
+          <input
+            type="text"
+            disabled
+            id="rgb"
+            placeholder="rgb"
+            value={this.state.rgb}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
